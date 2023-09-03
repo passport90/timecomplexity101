9931f2a4501ee3c748849ddff8d2646549234edc
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2023-09-03 16:25:06 +0700

    feat: logarithmic

---
+++ search/logarithmic.js
@@ -0,0 +1,42 @@
+const readline = require('readline')
+const needle = parseInt(process.argv[2])
+if (isNaN(needle)) {
+  throw new Error('Needle is undefined!')
+}
+
+const main = async () => {
+  const rl = readline.createInterface({ input: process.stdin, crlfDelay: Infinity })
+
+  const elements = []
+  for await (const line of rl) {
+    elements.push(parseInt(line))
+  }
+
+  const start = Date.now()
+
+  let index = undefined
+  let left = 0
+  let right = elements.length - 1
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+    const midElement = elements[mid]
+    if (midElement < needle) {
+      left = mid + 1
+    } else if (needle < midElement) {
+      right = mid - 1
+    } else {
+      index = mid
+      break
+    }
+  }
+
+  if (index !== undefined) {
+    console.info(`Found at index: ${index}!`)
+  } else {
+    console.info('Not found!')
+  }
+
+  console.info(`Time elapsed: ${Date.now() - start} ms.`)
+}
+
+main()
