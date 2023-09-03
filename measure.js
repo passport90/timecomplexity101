4935c3cae31272b2ef75a4378fd17e59fb83cb12
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2023-09-03 22:02:52 +0700

    feat: measure hof

---
+++ measure.js
@@ -0,0 +1,7 @@
+const measureTimeElapsed = (func) => {
+  const start = process.hrtime.bigint()
+  func()
+  console.info(`Time elapsed: ${(process.hrtime.bigint() - start) / BigInt(1_000)} microseconds.`)
+}
+
+module.exports = { measureTimeElapsed }
