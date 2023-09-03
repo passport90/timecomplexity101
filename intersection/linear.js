1a0f2db68a78bd4ad1d2b507219e44b65dcc4831
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2023-09-03 17:11:39 +0700

    feat: linear

---
+++ intersection/linear.js
@@ -0,0 +1,31 @@
+const readline = require('readline')
+
+const main = async () => {
+  const rl = readline.createInterface({ input: process.stdin, crlfDelay: Infinity })
+
+  // ???
+
+  let isHaystack = true
+  for await (const line of rl) {
+    if (line === '-') {
+      isHaystack = false
+      continue
+    }
+
+    const element = parseInt(line)
+
+    // ???
+  }
+
+  const start = process.hrtime.bigint()
+
+  let intersectionCount = 0
+
+  // ???
+
+  console.info(`Intersection count: ${intersectionCount}.`)
+
+  console.info(`Time elapsed: ${(process.hrtime.bigint() - start) / BigInt(1000)} microseconds.`)
+}
+
+main()
