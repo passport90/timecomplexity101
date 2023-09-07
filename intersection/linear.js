2951a1954851dcc784f66ba660731dac7a88deef
wicaker <wicaksanaigede@gmail.com>
2023-09-07 15:40:58 +0800

    winwin solution

--- intersection/linear.js
+++ intersection/linear.js
@@ -4,7 +4,8 @@ const { measureTimeElapsed } = require('../lib/measure')
 const main = async () => {
   const rl = readline.createInterface({ input: process.stdin, crlfDelay: Infinity })
 
-  // ???
+  const haystackElements = []
+  const needleElements = []
 
   let isHaystack = true
   for await (const line of rl) {
@@ -14,18 +15,32 @@ const main = async () => {
     }
 
     const element = parseInt(line)
-
-    // ???
+    if (isHaystack) {
+      haystackElements.push(element)
+    } else {
+      needleElements.push(element)
+    }
   }
 
 
   let intersectionCount = 0
   measureTimeElapsed(() => {
-    // ???
-    throw new Error('Not implemented!')
+    intersectionCount = nestedIterationIntersectionLiniear(haystackElements, needleElements)
   })
 
   console.info(`Intersection count: ${intersectionCount}.`)
+}
+
+const nestedIterationIntersectionLiniear = (haystackElements, needleElements) => {
+  let intersectionCount = 0
+
+  const combinedArray = haystackElements.concat(needleElements);
+  const sortedArray = combinedArray.sort((a, b) => {
+    intersectionCount++;
+    return a - b
+  });
+
+  return intersectionCount
 }
 
 main()
